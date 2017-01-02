08c4752266ff522567c2eab15cef9c59f52453d7
zhang <411152544@qq.com>
2017-01-02 14:59:15 +0800

    log4js

--- app.js
+++ app.js
@@ -46,14 +46,27 @@ let
     //console.log('created: ' + JSON.stringify(cat));
 //})();
 
+//log工具
+const logUtil = require('./utils/log_util');
+
 //middleware
 app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    //console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     var
         start = new Date().getTime(),
         execTime;
-    await next();
-    execTime = new Date().getTime() - start;
+    try{
+        //开始进入到下一个中间件
+        await next();
+
+        execTime = new Date().getTime() - start;
+
+        logUtil.logResponse(ctx,execTime);
+    } catch(error){
+        execTime = new Date().getTime() - start;
+
+        logUtil.logError(ctx,error,execTime);
+    }
     ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
--- controllers/message.js
+++ controllers/message.js
@@ -2,50 +2,65 @@
  * Created by zhang on 2016/12/30.
  */
 
-const model = require('../model');
-var   Message   = model.Message;
+    const url = require('url');
 
-var fn_message = async(ctx,next)=>{
+    const model = require('../model');
+    var   Message   = model.Message;
 
-    var data = await Message.findAll({order:[['username','DESC']]});
-    //await Message.findOne().then(function(msg){
-    //    console.log(msg.dataValues);
-    //    data = msg.dataValues;
-    //});
-    var msg = [];
-    for(var v in data){
-        msg[v] = data[v]['dataValues'];
-        //console.log(msg);
-    }
+    var fn_message = async(ctx,next)=>{
 
-    //console.log('message '+ JSON.stringify(ctx.response));
-    ctx.render('message.html',{'title':'message','msg':msg});
-};
+        var data = await Message.findAll({order:[['username','DESC']]});
+        //await Message.findOne().then(function(msg){
+        //    console.log(msg.dataValues);
+        //    data = msg.dataValues;
+        //});
+        var msg = [];
+        for(var v in data){
+            msg[v] = data[v]['dataValues'];
+            //console.log(msg);
+        }
 
-var fn_addMessage = async(ctx,next)=>{
-    //console.log('message '+ JSON.stringify(ctx.request));
-    var username = ctx.request.body.username || "",
-        title    = ctx.request.body.title || "",
-        context  = ctx.request.body.context || "",
-        email    = ctx.request.body.email || "";
+        //console.log('message '+ JSON.stringify(ctx.response));
+        ctx.render('message.html',{'title':'message','msg':msg});
+    };
 
-    Message.create({
-        email:email,
-        username:username,
-        title:title,
-        context:context
-    }).then(function(p){
-        console.log('created.' + JSON.stringify(p));
-    }).catch(function(err){
-        console.log('failed: '+ err);
-    });
-    ctx.redirect('/message');
-    ctx.status = 301;
+    var fn_addMessage = async(ctx,next)=>{
+        //console.log('message '+ JSON.stringify(ctx.request));
+        var username = ctx.request.body.username || "",
+            title    = ctx.request.body.title || "",
+            context  = ctx.request.body.context || "",
+            email    = ctx.request.body.email || "";
 
-    //await next();
-};
+        Message.create({
+            email:email,
+            username:username,
+            title:title,
+            context:context
+        }).then(function(p){
+            console.log('created.' + JSON.stringify(p));
+        }).catch(function(err){
+            console.log('failed: '+ err);
+        });
+        ctx.redirect('/message');
+        ctx.status = 301;
 
-module.exports = {
-    'GET /message':fn_message,
-    'POST /addMessage':fn_addMessage
-};
+        //await next();
+    };
+    var fn_deleteMessage = async(ctx,next)=>{
+
+        var urlObj = url.parse(ctx.request.url)
+        console.log(urlObj);
+        var id = ctx.request.body;
+
+    };
+
+    var fn_editMessage = async(ctx,next)=>{
+
+    };
+
+    module.exports = {
+        'GET /message':fn_message,
+        'GET /deleteMessage/:id':fn_deleteMessage,
+        'GET /editMessage/:id':fn_editMessage,
+        'POST /addMessage':fn_addMessage
+    };
