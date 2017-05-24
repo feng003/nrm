85bee1dff4d1c6a1fbcec75a76a99b275d99ed5a
feng003 <411152544@qq.com>
2017-05-24 14:54:33 +0800

    seed data

--- middleware/seed.js
+++ middleware/seed.js
@@ -8,20 +8,13 @@ let
 console.log(Message);
 const seed = ()=>{
     return async (ctx,next) => {
-        // var user = await User.create({
-        //     name: 'John',
-        //     gender: false,
-        //     email: 'john-' + Date.now() + '@garfield.pet',
-        //     passwd: 'hahaha'
-        // });
-        console.log('created: ' + User);
-        // var Message = await Message.create({
-        //     ownerId: user.id,
-        //     name: 'Garfield',
-        //     gender: false,
-        //     birth: '2007-07-07',
-        // });
-        console.log('created: ' + Message);
+        var user = await User.create({
+            name: 'John',
+            gender: false,
+            email: 'john-' + Date.now() + '@garfield.pet',
+            passwd: 'hahaha'
+        });
+        console.log('created: ' + JSON.stringify(user));
 
         await next();
     }
